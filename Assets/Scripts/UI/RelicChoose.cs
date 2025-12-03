0641d7eac8b23998b9410f81b17401cce6a43ed0
200IQpotato <aaronkuo0812@gmail.com>
2025-12-03 20:41:56 +0800

    add relic name

--- Assets/Scripts/UI/RelicChoose.cs
+++ Assets/Scripts/UI/RelicChoose.cs
@@ -5,11 +5,13 @@ public class RelicChoose : MonoBehaviour
 {
     [SerializeField] private Text text;
     [SerializeField] private Image image;
+    [SerializeField] private Text relicName;
     [SerializeField] public Image selectedImage;
 
     public void Init( RelicObject relicObject )
     {
         text.text = relicObject.description;
         image.sprite = relicObject.icon;
+        relicName.text = relicObject.relicName;
     }
 }
