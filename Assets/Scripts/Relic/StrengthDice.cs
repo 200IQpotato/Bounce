f81f75131fbfbb5f334c669299b8c431b6929863
Kuo aaron <aaronkuo0812@gmail.com>
2026-06-23 23:38:59 +0800

    add Strength Dice

---
+++ Assets/Scripts/Relic/StrengthDice.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Strength Dice", menuName = "Relics/Strength Dice")]
+public class StrengthDice : RelicObject
+{
+    public EffectObject strengthEffect;
+    public int maxStack;
+    public int maxDuration;
+
+    public override void OnTurnStart(Player player)
+    {
+        player.stats.ApplyEffect(new Effect(strengthEffect, Random.Range(0, maxDuration+1), Random.Range(0, maxStack+1))); 
+    }
+}
